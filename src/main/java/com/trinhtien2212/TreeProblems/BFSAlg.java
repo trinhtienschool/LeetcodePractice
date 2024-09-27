f7fdd14584a99e9265d84e16edd147bed8094699
Trinh Tien <trinhtien2212@gmail.com>
2024-09-27 07:14:02 +0700

    feat: reverse level order traversal

--- src/main/java/com/trinhtien2212/TreeProblems/BFSAlg.java
+++ src/main/java/com/trinhtien2212/TreeProblems/BFSAlg.java
@@ -4,33 +4,96 @@ import java.util.*;
 
 public class BFSAlg {
     /**
-     *Binary Tree Level Order Traversal
+     * Binary Tree Level Order Traversal
+     * Leetcode link: https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+     *
      * @param root
      * @return
      */
-    public static List<List<Integer>> levelOrder(TreeNode root){
-        if(Objects.isNull(root)) return null;
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        if (Objects.isNull(root)) return null;
         Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> result = new LinkedList<>();
         queue.add(root);
-       while(!queue.isEmpty()){
-           List<Integer> level = new ArrayList<>();
-           int size = queue.size();
-           for(int i = 0; i< size; i++){
-               TreeNode node = queue.poll();
-               level.add(node.val);
-               if(node.left != null) queue.add(node.left);
-               if(node.right != null) queue.add(node.right);
-           }
-           result.add(level);
-       }
-       return result;
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+
+    /**
+     * Practice level Order
+     * Flow:
+     * init a queue, add root to queue
+     * iterate the queue until the queue is empty
+     * new a list
+     * get all the same level of nodes of queue and queue its child node
+     * add node value to list
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder2(TreeNode root) {
+
+        //init a queue of TreeNode
+
+
+        if (Objects.isNull(root)) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> values = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                values.add(node.val);
+                if (Objects.nonNull(node.left)) queue.add(node.left);
+                if (Objects.nonNull(node.right)) queue.add(node.right);
+            }
+            result.add(values);
+        }
+        return result;
+    }
+
+    /**
+     * Binary Tree Level Order Traversal II
+     * Leetcode link: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/submissions/1403443889/
+     * @param root
+     */
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (Objects.isNull(root)) return new ArrayList<>();
+
+        LinkedList<List<Integer>> result = new LinkedList<List<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> nodes = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                nodes.add(node.val);
+                if (Objects.nonNull(node.left)) queue.add(node.left);
+                if (Objects.nonNull(node.right)) queue.add(node.right);
+            }
+            result.addFirst(nodes);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-     Integer[] array = {3,9,20,null,null,15,7};
-     TreeNode root = ArrayToTreeConvertor.convertTree(array);
-     List<List<Integer>> result = levelOrder(root);
-     System.out.println(result);
+        Integer[] array = {3, 9, 20, null, null, 15, 7};
+        TreeNode root = ArrayToTreeConvertor.convertTree(array);
+//        List<List<Integer>> result = levelOrder2(root);
+        List<List<Integer>> result = levelOrderBottom(root);
+        System.out.println(result);
     }
 }
